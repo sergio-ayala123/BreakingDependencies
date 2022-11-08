dfcbf9cc99ee1bb7ab15a246832361c21293d4a5
sergio-ayala123 <venturian6@gmail.com>
2022-11-07 23:56:53 -0700

    tests pass

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,6 +1,7 @@
 using ClassLibrary1;
 using Moq;
 using WinFormsApp1;
+using static WinFormsApp1.Form1;
 
 namespace TestProject1
 {
@@ -13,30 +14,46 @@ namespace TestProject1
         }
 
         [Test]
-        public void TestAddBananaMoreThanInventory()
-        {
-        }
-        [Test]
-        public void TestAddBanana()
-        {
-        }
-
-        [Test]
-        public void MultipleCarts()
-        {
-        }
-
-        [Test]
 
         public void CountButtonClick()
         {
+            FormTests form = new FormTests("You have to enter a name");
+            form.btnCalculate_Click(null, null);
+
             Assert.That(() => form.Message == "You have to enter a name");
         }
 
         [Test]
         public void NumBalanceError()
         {
+            FormTests form = new FormTests("Must be between 10k and 1MM");
+            form.btnCalculate_Click(null, null);
             Assert.That(() => form.Message == "Must be between 10k and 1MM");
+        }
+    }
+    public class FormTests: IFormDependancy
+    {
+        Form1 form = new Form1();
+        public string Message { get; set; }
+
+        public FormTests()
+        {
+
+        }
+        public FormTests(string message)
+        {
+            Show(message);
+        }
+
+
+        public void Show(string message)
+        {
+            Message = message;
+        }
+
+        public string btnCalculate_Click(object sender, EventArgs s)
+        {
+            return "button clicked";
         }
     }
 }
--- WinFormsApp1/Form1.cs
+++ WinFormsApp1/Form1.cs
@@ -7,23 +7,56 @@ namespace WinFormsApp1
             InitializeComponent();
         }
 
+        public interface IFormDependancy
+        { 
+            public void Show(string message);
+            public string btnCalculate_Click(object sender, EventArgs s);
+        }
+
+        public class FormDependancy : IFormDependancy
+        {
+            IFormDependancy Iform;
+            public FormDependancy()
+            {
+
+            }
+            public FormDependancy(IFormDependancy Iform)
+            {
+                this.Iform = Iform;
+            }
+
+            public string btnCalculate_Click(object sender, EventArgs s )
+            {
+                return "button Clicked";
+            }
+
+            public void Show(string message)
+            {
+                
+
+                MessageBox.Show(message);
+            }
+        }
+
+
         public void btnCalculate_Click(object sender, EventArgs e)
         {
+            FormDependancy form = new FormDependancy();
             if (txtName.Text.Length == 0)
             {
-                MessageBox.Show("You have to enter a name");
+                form.Show("You have to enter a name");
                 return;
             }
 
             if (numBalance.Value < 10_000 || numBalance.Value > 1_000_000)
             {
-                MessageBox.Show("Must be between 10k and 1MM");
+                form.Show("Must be between 10k and 1MM");
                 return;
             }
 
             if (File.Exists("authkey.txt") is false)
             {
-                MessageBox.Show("Missing authorization key");
+                form.Show("Missing authorization key");
                 return;
             }
 
